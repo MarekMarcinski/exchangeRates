72c66806162c550670adf36e299c6ab8800edeb6
Marek Marciński <marcinski.mar@gmail.com>
2018-10-26 23:02:46 +0200

    check of boundary conditions was added

--- src/main/java/org/marcinski/exchangeRates/controller/RateController.java
+++ src/main/java/org/marcinski/exchangeRates/controller/RateController.java
@@ -1,5 +1,6 @@
 package org.marcinski.exchangeRates.controller;
 
+import org.marcinski.exchangeRates.model.Table;
 import org.marcinski.exchangeRates.service.CurrencyValues;
 
 import javax.servlet.RequestDispatcher;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 
 @WebServlet("/rates")
 public class RateController extends HttpServlet {
@@ -19,23 +21,7 @@ public class RateController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         if (nonEmptyParameters(req)) {
-            String currencyCode = req.getParameter("currencyCode");
-            String startDate = req.getParameter("startDay");
-            String endDate = req.getParameter("endDay");
-
-            String url = String.format("http://api.nbp.pl/api/exchangerates/rates/a/%s/%s/%s/",
-                    currencyCode, startDate, endDate);
-
-            currencyValues.setUrl(url);
-
-            Double average = currencyValues.getAverageValueOfRates();
-            Double standatdDeviance = currencyValues.getStandardVariance();
-
-            if (average!=null) {
-                req.setAttribute("average", average);
-                req.setAttribute("standardDeviance", standatdDeviance);
-                req.setAttribute("currencyCode", currencyCode);
-            }
+            setRequestParameters(req);
         }
         RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
         dispatcher.forward(req, resp);
@@ -43,7 +29,85 @@ public class RateController extends HttpServlet {
 
     private boolean nonEmptyParameters(HttpServletRequest req) {
         return req.getParameter("currencyCode")!= null && !req.getParameter("currencyCode").isEmpty()
-            && req.getParameter("startDay")!= null && !req.getParameter("startDay").isEmpty()
-            && req.getParameter("endDay")!= null && !req.getParameter("endDay").isEmpty();
+                && req.getParameter("startDay")!= null && !req.getParameter("startDay").isEmpty()
+                && req.getParameter("endDay")!= null && !req.getParameter("endDay").isEmpty();
+    }
+
+    private void setRequestParameters(HttpServletRequest req) throws IOException {
+        String currencyCode = req.getParameter("currencyCode").trim();
+        String startDate = req.getParameter("startDay");
+        String endDate = req.getParameter("endDay");
+        String errorMsg;
+
+        if (isDataInCorrectFormat(startDate, endDate) &&
+                isCurrencyCodeCorrect(currencyCode) &&
+                isEndDateAfterStartDate(startDate, endDate)) {
+
+            String tableCode = getCodeOfRatesTable(currencyCode);
+
+            boolean isOk = true;
+            if (tableCode.equals("b")){
+                isOk = isWednesdayForStartAndEnd(startDate, endDate);
+            }
+
+            if (isOk) {
+                String url = String.format("http://api.nbp.pl/api/exchangerates/rates/%s/%s/%s/%s/",
+                        tableCode, currencyCode, startDate, endDate);
+
+                currencyValues.setUrl(url);
+
+                Double average = currencyValues.getAverageValueOfRates();
+                Double standardDeviance = currencyValues.getStandardVariance();
+
+                req.setAttribute("average", average);
+                req.setAttribute("standardDeviance", standardDeviance);
+                req.setAttribute("currencyCode", currencyCode);
+
+            }else {
+                errorMsg = "For table B you must provide a wednesday";
+                req.setAttribute("errorMsg", errorMsg);
+            }
+        }else if (!isDataInCorrectFormat(startDate, endDate)){
+            errorMsg = "Date in wrong format!";
+            req.setAttribute("errorMsg", errorMsg);
+        }else if (!isEndDateAfterStartDate(startDate, endDate)){
+            errorMsg = "End date must be after start date!";
+            req.setAttribute("errorMsg", errorMsg);
+        }else {
+            errorMsg = "Wrong code of currency!";
+            req.setAttribute("errorMsg", errorMsg);
+        }
+    }
+
+    private boolean isDataInCorrectFormat(String startDate, String endDate) {
+        return startDate.matches("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))") &&
+                endDate.matches("([12]\\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01]))");
+    }
+
+    private boolean isCurrencyCodeCorrect(String currencyCode) {
+        return Table.getTableA().contains(currencyCode.toUpperCase()) ||
+                Table.getTableB().contains(currencyCode.toUpperCase());
+    }
+
+    private boolean isEndDateAfterStartDate(String startDate, String endDate) {
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate end = LocalDate.parse(endDate);
+        return start.equals(end) || start.isBefore(end);
+    }
+
+    private String getCodeOfRatesTable(String currencyCode) {
+        String tableCode;
+        if (Table.getTableA().contains(currencyCode.toUpperCase())){
+            tableCode = "a";
+        }else {
+            tableCode = "b";
+        }
+        return tableCode;
+    }
+
+    private boolean isWednesdayForStartAndEnd(String startDate, String endDate) {
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate end = LocalDate.parse(endDate);
+        return start.getDayOfWeek().equals(end.getDayOfWeek());
     }
 }
